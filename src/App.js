8ba5d2da61d65b9638f5ecd539f08b800bb0003f
Corey Bailey <public@coreybailey.net>
2021-03-18 14:18:28 -0400

    Add header component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
+import Header from './component/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-      </Switch>
+       </Switch>
     </div>
   );
 }
