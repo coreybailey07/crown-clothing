78aeaa384dfcce5b575a2e66d33219bb6179bb99
Corey Bailey <public@coreybailey.net>
2021-03-31 11:21:14 -0400

    Update to previous commit * FIx classname

--- src/component/checkout-item/checkout-item.component.jsx
+++ src/component/checkout-item/checkout-item.component.jsx
@@ -4,7 +4,7 @@ import "./checkout-item.styles.scss";
 
 const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
   <div className="checkout-item">
-    <div className="image-counter">
+    <div className="image-container">
       <img src={imageUrl} alt="item" />
     </div>
     <span className="name">{name}</span>
