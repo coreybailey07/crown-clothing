4842c70eb0248e84c518271eb7438ac7a996bbfe
Corey Bailey <public@coreybailey.net>
2021-06-09 19:09:39 -0400

    Update README and store middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './root-reducer';
 const middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger)
+  middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
